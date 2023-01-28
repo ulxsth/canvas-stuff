0dc1913aa0873cf37d76c48e01ad4fe223d04a78
ulxsth <ulxsth@gmail.com>
2023-01-29 03:12:23 +0900

    #1 描画関数を実装

--- src/renderer.ts
+++ src/renderer.ts
@@ -7,7 +7,6 @@ export class Renderer {
         this.context = context;
     }
 
-    // TODO: 色適用
     public drawCircle(x: number, y: number, r: number, color: Color): void {
         const context = this.context;
         context.fillStyle = color.rgba;
@@ -15,5 +14,58 @@ export class Renderer {
         context.arc(x, y, r, 0, 2 * Math.PI);
         context.closePath();
         context.fill();
+    }
+
+    public drawLineCircle(
+        x: number,
+        y: number,
+        r: number,
+        lineWidth: number,
+        color: Color
+    ): void {
+        const context = this.context;
+        context.strokeStyle = color.rgba;
+        context.beginPath();
+        context.arc(x, y, r, 0, 2 * Math.PI);
+        context.closePath();
+        context.stroke();
+    }
+
+    public drawLine(
+        x1: number,
+        y1: number,
+        x2: number,
+        y2: number,
+        width: number,
+        color: Color
+    ) {
+        const context = this.context;
+        context.strokeStyle = color.rgba;
+        context.beginPath();
+        context.lineTo(x1, y1);
+        context.lineTo(x2, y2);
+        context.closePath();
+        context.lineWidth = width;
+        context.stroke();
+    }
+
+    public pushMatrix() {
+        this.context.save();
+    }
+
+    public popMatrix() {
+        this.context.restore();
+    }
+
+    public rotate(angle: number) {
+        this.context.rotate(angle);
+    }
+
+    public lineTo(x: number, y: number) {
+        this.context.lineTo(x, y);
+    }
+
+    public moveTo(x: number, y: number) {
+        this.context.lineTo(x, y);
     }
 }
