3abdd7cded66afceec9e0361e7db7e03309d8537
ulxsth <ulxsth@gmail.com>
2023-01-29 13:18:01 +0900

    #8 Shapeの実装

---
+++ src/core/Shape.ts
@@ -0,0 +1,12 @@
+import { Scene } from "./Scene";
+
+export class Shape {
+    public draw(
+        x: number,
+        y: number,
+        angle: number,
+        scale: number,
+        deltaTime: number,
+        scene: Scene
+    ) {}
+}
