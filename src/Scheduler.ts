05f7dd24e6bde18d4cc761607921b30e70107b58
ulxsth <ulxsth@gmail.com>
2023-01-29 09:15:19 +0900

    #3 スケジューラ実装

---
+++ src/Scheduler.ts
@@ -0,0 +1,15 @@
+export const schedule = (callback: (deltaTime: number) => void) => {
+    let time = Date.now();
+    let id = 0;
+    let loop = () => {
+        callback(Math.min(1, (Date.now() - time) * 0.001));
+        id = requestAnimationFrame(loop);
+        time = Date.now();
+    }
+    
+    loop();
+
+    return {
+        end: () => cancelAnimationFrame(id)
+    }
+}
